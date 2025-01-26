d8f1e442262595dcf94cd19221be3e9be7668f7b
AngelUPT <angel_1322134008@uptecamac.edu.mx>
2025-01-26 14:15:55 -0600

    adding actions

--- app/src/main/java/com/angelcorrea/formularioregistroactivity/MainActivity.kt
+++ app/src/main/java/com/angelcorrea/formularioregistroactivity/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.angelcorrea.formularioregistroactivity
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val edtNombre = findViewById<EditText>(R.id.edtNombre)
+        val edtEmail = findViewById<EditText>(R.id.edtEmail)
+        val edtPassword = findViewById<EditText>(R.id.edtPassword)
+        val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+
+        btnRegistrarse.setOnClickListener {
+            val nombre = edtNombre.text.toString()
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            if (nombre.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+                val message = "Nombre: $nombre\nEmail: $email\nPassword: $password"
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "completa todos los campos.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
